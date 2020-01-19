e0395ae878a10f01f4fb0c0577aad37d468dca01
ShayakSarkar <shayaksarkar2000@gmail.com>
2020-01-19 14:37:10 +0530

    SJF algo update

--- SJF.java
+++ SJF.java
@@ -31,18 +31,27 @@ public class SJF{
 	}
 
 	public static LinkedList<TimeSlice> performSJF(Job[] jobs){	
+		Scanner in=new Scanner(System.in);
 		//GantChart array will have the time slices for all the jobs
 		if(jobs.length==0)
 			throw new IllegalArgumentException("Job array has no values");
 		
-		Comparator<Job> comparator=new Comparator<Job>(){	// For comparing the values in Priority Queue
+		Comparator<Job> pqComparator=new Comparator<Job>(){	// For comparing the values in Priority Queue
 			public  int compare(Job job1,Job job2){
-				if(job1.bt-job2.bt<=EPS)
+				if(Math.abs(job1.bt-job2.bt)<=EPS)
 					return 0;
-				return (job1.bt-job2.bt)<0?-1:1;
+				return (job1.bt-job2.bt)>0?1:-1;
 			}
 		};
-		PriorityQueue<Job> pq=new PriorityQueue<Job>(comparator);	// Maintains the optimal jobs available
+		Comparator<Job> sortComparator=new Comparator<Job>(){
+			public int compare(Job job1,Job job2){
+				if(Math.abs(job1.at-job2.at)<=EPS)
+					return 0;
+				return (job1.at-job2.at>0)?1:-1;
+			}
+
+		};
+		PriorityQueue<Job> pq=new PriorityQueue<Job>(pqComparator);	// Maintains the optimal jobs available
 		
 		if(pq==null)
 			throw new IllegalArgumentException("Priority Queue could not be initialised");
@@ -52,32 +61,40 @@ public class SJF{
 		if(ganttChart==null)
 			throw new IllegalArgumentException("Gantt Chart could not be initialised");
 
-		System.out.println("Echo1");
-		Arrays.sort(jobs,comparator);
-		System.out.println("Echo2");
-		int jobno=0;		//Maintains the job number last pushed into the pq
-		pq.offer(jobs[jobno]);
-		double timeElapsed=jobs[jobno].at;
+		//System.out.println("Echo1");
+		Arrays.sort(jobs,sortComparator);
+		/*
+		for(Job job : jobs){
+			System.out.print(job.name+" ");
+		}
+		*/
+		//System.out.println("Echo2");
+		int jobno=-1;		//Maintains the job number last arrived in
+		double timeElapsed=0;
 		
 		//Execution until all jobs are in the priority queue
 		while(jobno<jobs.length-1){
-			System.out.println(timeElapsed);
+			//System.out.println(timeElapsed);
 			if(pq.isEmpty()){
 				timeElapsed=jobs[jobno+1].at;
-				pq.offer(jobs[jobno+1]);
-				jobno++;
+				while(jobno<jobs.length-1 && jobs[jobno+1].at==timeElapsed){
+					pq.offer(jobs[jobno+1]);
+					jobno++;
+				}
 				continue;
 			}
 			Job selected=pq.poll();
 			TimeSlice ts=new TimeSlice(selected.name,timeElapsed);
 			ganttChart.add(ts);
-			double duration=Math.min(selected.bt,jobs[jobno].at-timeElapsed);
+			double duration=Math.min(selected.bt,jobs[jobno+1].at-timeElapsed);
 			selected.bt-=duration;
 			if(!(selected.bt<=0))
 				pq.offer(selected);
 			timeElapsed+=duration;
-			jobno++;
-			pq.offer(jobs[jobno]);
+			while(jobno<jobs.length-1 && timeElapsed==jobs[jobno+1].at){
+				jobno++;
+				pq.offer(jobs[jobno]);
+			}
 		}
 
 		//Execution after all jobs are in the priority queue
@@ -87,16 +104,31 @@ public class SJF{
 			ganttChart.add(ts);
 			timeElapsed+=selected.bt;
 		}
-		return ganttChart;
+		return refineGanttChart(ganttChart);
 	
+	}
+	public static LinkedList<TimeSlice> refineGanttChart(LinkedList<TimeSlice> ganttChart){
+		int i=0;
+		LinkedList<TimeSlice> refinedGanttChart=new LinkedList<TimeSlice>();
+		while(i<ganttChart.size()){
+			refinedGanttChart.add(ganttChart.get(i));
+			String processName=ganttChart.get(i).jobName;
+			int j=i+1;
+			while(j<ganttChart.size() && processName==ganttChart.get(j).jobName){
+				j++;
+			}
+			i=j;
+		}
+		return refinedGanttChart;
+
 	}
 	public static void main(String args[]){
 		Job[] jobs=new Job[5];
-		jobs[0]=new Job(0,7,"P1");
-		jobs[1]=new Job(2,4,"P2");
-		jobs[2]=new Job(3,1,"P3");
-		jobs[3]=new Job(5,4,"P4");
-		//jobs[4]=new Job(5,8,"P5");
+		jobs[0]=new Job(0,12,"P2");
+		jobs[1]=new Job(3,8,"P3");
+		jobs[2]=new Job(5,4,"P4");
+		jobs[3]=new Job(10,10,"P1");
+		jobs[4]=new Job(12,6,"P5");
 		
 		System.out.println("Jobs Inititalised...");
 
--- test.java
+++ test.java
@@ -1,5 +1,15 @@
+import java.util.PriorityQueue;
+import java.util.LinkedList;
+
 public class test{
 	public static void main(String args[]){
-		(2<4)?true:return;
+		LinkedList<Integer> ll=new LinkedList<Integer>();
+		ll.add(1);
+		ll.add(2);
+		ll.add(3);
+		ll.add(4);
+		System.out.println(ll);
+		ll.remove(2);
+		System.out.println(ll);
 	}
 }
